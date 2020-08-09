706953ab133595f559fb997b351fc453cec9f82d
Matthias Bühlmann <matthias.buehlmann@mabulous.com>
2020-08-09 17:57:00 +0200

    Initial commit, ESP-32 specific.

---
+++ MabuTrace/mabutrace.c
@@ -0,0 +1,291 @@
+#include "mabutrace.h"
+
+#include <string.h>
+
+#include "esp_timer.h"
+
+#define configUSE_TRACE_FACILITY 1
+#define traceTASK_DELAY_UNTIL() TRACE_SCOPE("vTaskDelayUntil")
+#define traceTASK_DELAY() TRACE_SCOPE("vTaskDelay")
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static void* profiler_entries = NULL;
+static volatile size_t entries_start_index = 0;
+static volatile size_t entries_next_index = 0;
+static volatile portMUX_TYPE profiler_index_mutex = portMUX_INITIALIZER_UNLOCKED;
+static volatile uint16_t link_index = 0;
+static volatile portMUX_TYPE link_index_mutex = portMUX_INITIALIZER_UNLOCKED;
+static volatile TaskHandle_t task_handles[16];
+static volatile uint8_t type_sizes[8];
+
+
+void profiler_init() {
+  if(profiler_entries)
+    return;
+#ifdef USE_PSRAM_IF_AVAILABLE
+  profiler_entries = heap_caps_calloc(PROFILER_BUFFER_SIZE_IN_BYTES, 1, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+#endif
+  if(!profiler_entries)
+    profiler_entries = calloc(PROFILER_BUFFER_SIZE_IN_BYTES, 1);
+  if (!profiler_entries)
+    ets_printf("Failed to allocate trace buffer.\n");
+  else
+    ets_printf("Allocated %d bytes for trace buffer.\n", (int)PROFILER_BUFFER_SIZE_IN_BYTES);
+  memset(task_handles, 0, sizeof(task_handles));
+
+  memset(type_sizes, 0, sizeof(type_sizes));
+  type_sizes[EVENT_TYPE_NONE] = 0;
+  type_sizes[EVENT_TYPE_DURATION] = sizeof(duration_entry_t);
+  type_sizes[EVENT_TYPE_DURATION_COLORED] = sizeof(duration_colored_entry_t);
+  type_sizes[EVENT_TYPE_INSTANT_COLORED] = sizeof(instant_colored_entry_t);
+  type_sizes[EVENT_TYPE_COUNTER] = sizeof(counter_entry_t);
+  type_sizes[EVENT_TYPE_LINK] = sizeof(link_entry_t);
+}
+
+void profiler_deinit() {
+  if(!profiler_entries)
+    return;
+  taskENTER_CRITICAL(&profiler_index_mutex);
+  free(profiler_entries);
+  profiler_entries = NULL;
+  taskEXIT_CRITICAL(&profiler_index_mutex);
+}
+
+inline TaskHandle_t IRAM_ATTR get_current_task_handle() {
+  if (xPortInIsrContext()) {
+    return NULL;
+  }
+  else {
+    return xTaskGetCurrentTaskHandle();
+  }
+}
+
+inline TaskHandle_t IRAM_ATTR get_task_handle_from_id(uint8_t id) {
+  return task_handles[id];
+}
+
+inline uint8_t IRAM_ATTR get_current_task_id() {
+  TaskHandle_t handle = get_current_task_handle();
+  if (!handle) {
+    return 0;
+  } else {
+    for (uint8_t i = 1; i < 16; i++) {
+      TaskHandle_t* handle_i = &task_handles[i];
+      if (!*handle_i) {
+        *handle_i = handle;
+        return i;
+      } else if (*handle_i == handle) {
+        return i;
+      }
+    }
+  }
+  assert(false); // never get here.
+  return 0;
+}
+
+inline void IRAM_ATTR advance_pointers(uint8_t type_size, size_t* out_entry_idx) {
+  if(!profiler_entries)
+    return;
+  taskENTER_CRITICAL(&profiler_index_mutex);
+  {
+    assert(entries_next_index <= PROFILER_BUFFER_SIZE_IN_BYTES);
+    //critical section
+    size_t start_idx = 0;
+    size_t entry_idx = entries_next_index;
+    //advance pointers
+    if (PROFILER_BUFFER_SIZE_IN_BYTES - entry_idx < type_size) {
+      // entry doesn't fit into end of buffer.
+      // clear tail to indicate end.
+      memset(profiler_entries + entries_next_index, 0, PROFILER_BUFFER_SIZE_IN_BYTES - entry_idx);
+      // set entry_idx to start of buffer.
+      entry_idx = 0;
+      start_idx = 0;
+      entries_next_index = type_size;
+    }
+    else {
+      // fits in.
+      start_idx = entries_start_index;
+      entries_next_index = entry_idx + type_size;
+    }
+    // advance start_idx
+    while (start_idx >= entry_idx && start_idx < entries_next_index) {
+      entry_header_t* start_header = (entry_header_t*)(profiler_entries + start_idx);
+      if (start_header->type == EVENT_TYPE_NONE) {
+        start_idx = 0;
+        break;
+      }
+      else {
+        start_idx += type_sizes[start_header->type];
+      }
+    }
+    if (start_idx == PROFILER_BUFFER_SIZE_IN_BYTES) {
+      start_idx = 0;
+    }
+    entries_start_index = start_idx;
+    *out_entry_idx = entry_idx;
+  }
+  taskEXIT_CRITICAL(&profiler_index_mutex);
+}
+
+inline void IRAM_ATTR insert_link_event(uint16_t link, uint8_t link_type, uint64_t time_stamp, uint8_t cpu_id, uint8_t task_id) {
+  if(!profiler_entries)
+    return;
+  size_t type_size = sizeof(link_entry_t);
+  size_t entry_idx = 0;
+  advance_pointers(type_size, &entry_idx);
+  link_entry_t* entry = (link_entry_t*)(profiler_entries + entry_idx);
+  entry->header.type = EVENT_TYPE_LINK;
+  entry->header.cpu_id = cpu_id;
+  entry->header.task_id = task_id;
+  entry->time_stamp_begin_microseconds = (uint32_t)time_stamp;
+  entry->link = link;
+  entry->link_type = link_type;
+}
+
+void profiler_get_entries(void* output_buffer, size_t* out_start_idx, size_t* out_end_idx) {
+  if(!profiler_entries)
+    return;
+  taskENTER_CRITICAL(&profiler_index_mutex);
+    memcpy(output_buffer, profiler_entries, PROFILER_BUFFER_SIZE_IN_BYTES);
+    *out_start_idx = entries_start_index;
+    *out_end_idx = entries_next_index;
+  taskEXIT_CRITICAL(&profiler_index_mutex);
+}
+
+void profiler_get_task_handles(void* output_taskhandle_16) {
+  memcpy(output_taskhandle_16, task_handles, sizeof(TaskHandle_t) * 16);
+}
+
+profiler_duration_handle_t IRAM_ATTR trace_begin(const char* name, uint8_t color) {
+  return trace_begin_linked(name, 0, NULL, color);
+}
+
+profiler_duration_handle_t IRAM_ATTR trace_begin_linked(const char* name, uint16_t link_in, uint16_t* link_out, uint8_t color) {
+  profiler_duration_handle_t result;
+  if(!profiler_entries)
+    return result;
+  result.time_stamp_begin_microseconds = esp_timer_get_time();
+  result.name = name;
+  result.link_in = link_in;
+  result.color = color;
+  if (link_out) {
+    if (*link_out == 0) {
+      taskENTER_CRITICAL(&link_index_mutex);
+        //critical section
+        result.link_out = ++link_index;
+      taskEXIT_CRITICAL(&link_index_mutex);
+      *link_out = result.link_out;
+    } else {
+      result.link_out = *link_out;
+    }
+  }
+  else {
+    result.link_out = 0;
+  }
+  return result;
+}
+
+void IRAM_ATTR trace_end(profiler_duration_handle_t* handle) {
+  if(!profiler_entries)
+    return;
+  uint8_t task_id = get_current_task_id();
+  uint8_t cpu_id = (uint8_t)xPortGetCoreID();
+  uint64_t now = esp_timer_get_time();
+  size_t type_size = 0;
+  if (handle->color == 0) {
+    type_size = sizeof(duration_entry_t);
+  } else {
+    type_size = sizeof(duration_colored_entry_t);
+  }
+
+  size_t entry_idx = 0;
+  advance_pointers(type_size, &entry_idx);
+  
+  uint64_t duration = now - handle->time_stamp_begin_microseconds;
+  if (handle->color == 0) {
+    duration_entry_t* entry = (duration_entry_t*)(profiler_entries + entry_idx);
+    entry->header.type = EVENT_TYPE_DURATION;
+    entry->header.cpu_id = cpu_id;
+    entry->header.task_id = task_id;
+    entry->time_stamp_begin_microseconds = (uint32_t)handle->time_stamp_begin_microseconds;
+    entry->time_duration_microseconds = duration;
+    entry->name = handle->name;
+  } else {
+    duration_colored_entry_t* entry = (duration_colored_entry_t*)(profiler_entries + entry_idx);
+    entry->header.type = EVENT_TYPE_DURATION_COLORED;
+    entry->header.cpu_id = cpu_id;
+    entry->header.task_id = task_id;
+    entry->time_stamp_begin_microseconds = (uint32_t)handle->time_stamp_begin_microseconds;
+    entry->time_duration_microseconds = duration;
+    entry->name = handle->name;
+    entry->color = handle->color;
+  }
+  if (handle->link_in) {
+    insert_link_event(handle->link_in, LINK_TYPE_IN, handle->time_stamp_begin_microseconds-1, cpu_id, task_id);
+  }
+  if (handle->link_out) {
+    insert_link_event(handle->link_out, LINK_TYPE_OUT, handle->time_stamp_begin_microseconds + duration - 1, cpu_id, task_id);
+  }
+}
+
+void IRAM_ATTR trace_instant(const char* name, uint8_t color) {
+  trace_instant_linked(name, 0, NULL, color);
+}
+
+void IRAM_ATTR trace_instant_linked(const char* name, uint16_t link_in, uint16_t* link_out, uint8_t color) {
+  if(!profiler_entries)
+    return;
+  uint8_t task_id = get_current_task_id();
+  uint8_t cpu_id = (uint8_t)xPortGetCoreID();
+  uint64_t now = esp_timer_get_time();
+  size_t type_size = sizeof(instant_colored_entry_t);
+
+  size_t entry_idx = 0;
+  advance_pointers(type_size, &entry_idx);
+
+  instant_colored_entry_t* entry = (instant_colored_entry_t*)(profiler_entries + entry_idx);
+  entry->header.type = EVENT_TYPE_INSTANT_COLORED;
+  entry->header.cpu_id = cpu_id;
+  entry->header.task_id = task_id;
+  entry->time_stamp_begin_microseconds = (uint32_t)now;
+  entry->name = name;
+  entry->color = color;
+
+  if (link_out) {
+    if (*link_out == 0) {
+      taskENTER_CRITICAL(&link_index_mutex);
+      //critical section
+        *link_out = ++link_index;
+      taskEXIT_CRITICAL(&link_index_mutex);
+    }
+  }
+
+  if (link_in) {
+    insert_link_event(link_in, LINK_TYPE_IN, now, cpu_id, task_id);
+  }
+  if (link_out && *link_out) {
+    insert_link_event(*link_out, LINK_TYPE_OUT, now, cpu_id, task_id);
+  }
+}
+
+void IRAM_ATTR trace_counter(const char* name, int32_t value, uint8_t color) {
+  if(!profiler_entries)
+    return;
+  uint8_t task_id = get_current_task_id();
+  uint8_t cpu_id = (uint8_t)xPortGetCoreID();
+  uint64_t now = esp_timer_get_time();
+  size_t type_size = sizeof(counter_entry_t);
+
+  size_t entry_idx = 0;
+  advance_pointers(type_size, &entry_idx);
+
+  counter_entry_t* entry = (counter_entry_t*)(profiler_entries + entry_idx);
+  entry->header.type = EVENT_TYPE_COUNTER;
+  entry->header.cpu_id = cpu_id;
+  entry->header.task_id = task_id;
+  entry->time_stamp_begin_microseconds = (uint32_t)now;
+  entry->name = name;
+  entry->value = value;
+}
---
+++ MabuTrace/mabutrace.h
@@ -0,0 +1,166 @@
+#ifndef __MABUTRACE_H__
+#define __MABUTRACE_H__
+
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+* Size of the circular buffer.
+*/
+#define USE_PSRAM_IF_AVAILABLE
+#define PROFILER_BUFFER_SIZE_IN_BYTES 65536 // 64kb
+
+/*
+* Predefined colors.
+*/
+#define COLOR_UNDEFINED      0x00  /* Let the visualizer choose a color */
+#define COLOR_GREEN        0x01
+#define COLOR_LIGHT_GREEN    0x02    
+#define COLOR_DARK_ORANGE    0x03
+#define COLOR_DARK_RED      0x04
+#define COLOR_YELLOW      0x05
+#define COLOR_OLIVE        0x06
+#define COLOR_BLACK        0x07
+#define COLOR_WHITE        0x08
+#define COLOR_GRAY        0x09
+#define COLOR_LIGHT_GRAY    0x0A
+
+/*
+* The following macros should be used for tracing ([] denotes optional argument).
+* Note that name is NOT copied, meaning that the pointer should remain valid. For this reason it's recommended
+* that the macros are only used with string literals as name.
+*
+* TRACE_SCOPE(const char* name, [uint8_t color]);
+* TRACE_SCOPE_LINKED(const char* name, uint16_t link_in, uint16_t* link_out, [uint8_t color]);
+* TRACE_INSTANT(const char* name, [uint8_t color]);
+* TRACE_COUNTER(const char* name, int24_t value, [uint8_t color]);
+*/
+
+
+#define _OVERLOAD_MACRO(_1,_2,_3, _4, NAME,...) NAME
+
+#define TRACE_SCOPE(...) _OVERLOAD_MACRO(__VA_ARGS__, 0, 0, _TRACE_SCOPE_COLORED, _TRACE_SCOPE_UNCOLORED)(__VA_ARGS__)
+#define TRACE_SCOPE_LINKED(...) _OVERLOAD_MACRO(__VA_ARGS__, _TRACE_SCOPE_LINKED_COLORED, _TRACE_SCOPE_LINKED_UNCOLORED, 0, 0)(__VA_ARGS__)
+#define TRACE_INSTANT(...) _OVERLOAD_MACRO(__VA_ARGS__, 0, 0, _TRACE_INSTANT_COLORED, _TRACE_INSTANT_UNCOLORED)(__VA_ARGS__)
+#define TRACE_COUNTER(...) _OVERLOAD_MACRO(__VA_ARGS__, 0, _TRACE_COUNTER_COLORED, _TRACE_COUNTER_UNCOLORED, 0)(__VA_ARGS__)
+
+#define _TRACE_INSTANT_UNCOLORED(name) trace_instant(name, COLOR_UNDEFINED);
+#define _TRACE_INSTANT_COLORED(name, color) trace_instant(name, color);
+#define _TRACE_INSTANT_LINKED(name, link_in, link_out, color) trace_instant(name, link_in, link_out, color);
+#define _TRACE_COUNTER_UNCOLORED(name, value) trace_counter(name, value, COLOR_UNDEFINED);
+#define _TRACE_COUNTER_COLORED(name, value, color) trace_counter(name, value, color);
+
+#ifdef __cplusplus
+#define _TRACE_SCOPE_UNCOLORED(name) Profiler scope_trace_helper_object(name, COLOR_UNDEFINED);
+#define _TRACE_SCOPE_COLORED(name, color) Profiler scope_trace_helper_object(name, color);
+#define _TRACE_SCOPE_LINKED_UNCOLORED(name, link_in, link_out) Profiler scope_trace_helper_object(name, link_in, link_out, COLOR_UNDEFINED);
+#define _TRACE_SCOPE_LINKED_COLORED(name, link_in, link_out, color) Profiler scope_trace_helper_object(name, link_in, link_out, color);
+extern "C" {
+#else
+#define _TRACE_SCOPE_UNCOLORED(name) profiler_duration_handle_t scope_trace_helper_handle __attribute__ ((__cleanup__(trace_end))) = trace_begin(name, COLOR_UNDEFINED);
+#define _TRACE_SCOPE_COLORED(name, color) profiler_duration_handle_t scope_trace_helper_handle __attribute__ ((__cleanup__(trace_end))) = trace_begin(name, color);
+#define _TRACE_SCOPE_LINKED_UNCOLORED(name, link_in, link_out) profiler_duration_handle_t scope_trace_helper_handle __attribute__ ((__cleanup__(trace_end))) = trace_begin_linked(name, link_in, link_out, COLOR_UNDEFINED);
+#define _TRACE_SCOPE_LINKED_COLORED(name, link_in, link_out, color) profiler_duration_handle_t scope_trace_helper_handle __attribute__ ((__cleanup__(trace_end))) = trace_begin_linked(name, link_in, link_out, color);
+#endif
+
+typedef struct {
+  uint64_t time_stamp_begin_microseconds;
+  const char* name;
+  uint16_t link_in;
+  uint16_t link_out;
+  uint8_t color; 
+} profiler_duration_handle_t;
+
+typedef struct {
+  uint8_t type : 3;    // 2^3 = 8 different event types.
+  uint8_t cpu_id : 1;  // 2 cpus.
+  uint8_t task_id : 4;  // 2^4 = 16 different tasks.
+} __attribute__((packed)) entry_header_t;
+#define EVENT_TYPE_NONE 0
+
+typedef struct {
+  entry_header_t header;
+  unsigned int time_duration_microseconds : 24;  /* Duration of event in microseconds. 24bit yields up to 16 seconds duration.*/
+  uint32_t time_stamp_begin_microseconds;      /* Start of event start in microseconds since device started. 32bit overflows every 70 minutes. */
+  const char* name;                /* Name of the event. */
+} __attribute__((packed)) duration_entry_t;
+#define EVENT_TYPE_DURATION 1
+
+typedef struct {
+  entry_header_t header;
+  uint8_t color;
+  unsigned int time_duration_microseconds;    /* Duration of event in microseconds. */
+  uint32_t time_stamp_begin_microseconds;      /* Start of event start in microseconds since device started. 32bit overflows every 70 minutes. */
+  const char* name;                /* Name of the event. */
+} __attribute__((packed)) duration_colored_entry_t;
+#define EVENT_TYPE_DURATION_COLORED 2
+
+typedef struct {
+  entry_header_t header;
+  uint8_t color;
+  uint32_t time_stamp_begin_microseconds;      /* Start of event start in microseconds since device started. 32bit overflows every 70 minutes. */
+  const char* name;                /* Name of the event. */
+} __attribute__((packed)) instant_colored_entry_t;
+#define EVENT_TYPE_INSTANT_COLORED 3
+
+typedef struct {
+  entry_header_t header;
+  signed int value : 24;              /* 24 bits allows for values between -8388608 and 8388607*/
+  uint32_t time_stamp_begin_microseconds;      /* Start of event start in microseconds since device started. 32bit overflows every 70 minutes. */
+  const char* name;                /* Name of the event. */
+} __attribute__((packed)) counter_entry_t;
+#define EVENT_TYPE_COUNTER 4
+
+typedef struct {
+  entry_header_t header;
+  uint8_t link_type;                /* 0: in, 1: out*/
+  uint16_t link;                  /* Link id */
+  uint32_t time_stamp_begin_microseconds;      /* Start of event start in microseconds since device started. 32bit overflows every 70 minutes. */
+} __attribute__((packed)) link_entry_t;
+#define EVENT_TYPE_LINK 5
+#define LINK_TYPE_IN 0
+#define LINK_TYPE_OUT 1
+
+typedef struct {
+  uint8_t type;  /* Type of event. Based on this type, different fields from the union part are valid. */
+  uint8_t cpu_id;  /* ID of CPU from which event was traced. */
+  uint8_t color;  /* One of a few predefined color values. */
+  uint16_t link_in;  /* Flow Event id's to visualize links between events. */
+  uint16_t link_out;
+  void* task_handle;  /* FreeRTOS task handle. NULL if called from interrupt. */
+  const char* name;  /* Name of the event. */
+  uint32_t time_stamp_begin_microseconds; /* Start of event start in microseconds since device started. */
+  union {
+    struct {  // EVENT_TYPE_DURATION additional fields.
+      uint32_t time_duration_microseconds; /* Duration of event in microseconds. */
+    };
+    struct {  // EVENT_TYPE_COUNTER additional fields.
+      int32_t counter_value;  /* Value of the counter to keep track of. */
+    };
+  };
+} profiler_entry_t;
+
+void profiler_init();
+void profiler_deinit();
+void profiler_get_entries(void* output_buffer, size_t* out_start_idx, size_t* out_end_idx);
+void profiler_get_task_handles(void* output_taskhandle_16);
+profiler_duration_handle_t trace_begin(const char* name, uint8_t color);
+profiler_duration_handle_t trace_begin_linked(const char* name, uint16_t link_in, uint16_t* link_out, uint8_t color);
+void trace_end(profiler_duration_handle_t* handle);
+void trace_instant(const char* name, uint8_t color);
+void trace_instant_linked(const char* name, uint16_t link_in, uint16_t* link_out, uint8_t color);
+void trace_counter(const char* name, int32_t value, uint8_t color);
+
+#ifdef __cplusplus
+class Profiler {
+public:
+  Profiler(const char* name, uint8_t color) { _handle = trace_begin(name, color); }
+  Profiler(const char* name, uint16_t link_in, uint16_t* link_out, uint8_t color) { _handle = trace_begin_linked(name, link_in, link_out, color); }
+  ~Profiler() { trace_end(&_handle); }
+private:
+  profiler_duration_handle_t _handle;
+};
+}
+#endif
+
+#endif  //__MABUTRACE_H__
